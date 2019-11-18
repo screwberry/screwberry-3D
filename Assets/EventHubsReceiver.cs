ba2fabaca8754c1597e8551ea8709d274a2e1868
screwberry <mikko.m.kallio@gmail.com>
2019-11-18 19:08:07 +0200

    Downgrade to 2018.2 and integrate Iot Hub connection

---
+++ Assets/EventHubsReceiver.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Text;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+using System;
+using UnityEngine.UI;
+using Microsoft.Azure.EventHubs;
+using Newtonsoft.Json.Linq;
+
+public class EventHubsReceiver : MonoBehaviour
+{
+	private static EventHubClient eventHubClient;
+    private static string ConnectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING");
+    private static string EntityPath = Environment.GetEnvironmentVariable("ENTITY_PATH");
+
+    void Start() {
+		var connectionStringBuilder = new EventHubsConnectionStringBuilder(ConnectionString){EntityPath = EntityPath};
+		eventHubClient = EventHubClient.CreateFromConnectionString(connectionStringBuilder.ToString());
+        Initialize();
+    }
+
+    async void Initialize(){
+        var runtimeInfo = await eventHubClient.GetRuntimeInformationAsync();
+        var d2cPartitions = runtimeInfo.PartitionIds;
+
+        CancellationTokenSource cts = new CancellationTokenSource();
+
+        foreach (string partition in d2cPartitions)
+        {
+            ReceiveMessagesFromDeviceAsync(partition, cts.Token);
+        }
+
+        //await eventHubClient.CloseAsync();
+    }
+
+    private static async Task ReceiveMessagesFromDeviceAsync(string partition, CancellationToken ct)
+    {
+        var eventHubReceiver = eventHubClient.CreateReceiver("receive", partition, EventPosition.FromEnqueuedTime(DateTime.Now.AddMinutes(-10)));
+        while (true)
+        {
+            if (ct.IsCancellationRequested) break;
+            var events = await eventHubReceiver.ReceiveAsync(20);
+
+            if (events != null) {
+                foreach(EventData eventData in events)
+                {
+                    var data = JObject.Parse(Encoding.UTF8.GetString(eventData.Body.Array));
+                    var label = GameObject.Find(data["alias"].ToString()).GetComponent<Text>();
+                    label.text = data["alias"]
+                    + "\n" + data["temperature"].ToString() + " °C"
+                    + "\n" + data["humidity"].ToString() + "%";
+                    Debug.Log(data);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/LabelController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LabelController : MonoBehaviour {
+
+	public Transform ruuvi;
+	public Camera camera;
+	void Update () {
+		Vector3 screenPos = camera.WorldToScreenPoint(ruuvi.position);
+        this.transform.position = new Vector3(screenPos.x,screenPos.y+20,0);
+	}
+}
