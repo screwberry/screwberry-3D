37af1356d13c9259ee85550ea70b2a72ea4e3817
screwberry <mikko.m.kallio@gmail.com>
2019-11-19 07:23:45 +0200

    Add camera control

---
+++ Assets/CamControl.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CamControl : MonoBehaviour {
+
+	public float camSpeed = 30.00f;
+	public float smoothing = 10.0f;
+	private float angle;
+
+	void Update() {
+		angle += camSpeed * Input.GetAxis("Mouse X");
+		Vector3 targetPos = new Vector3(0f, angle, 0f);
+		Vector3 smoothPos = Vector3.Lerp(transform.eulerAngles, targetPos, smoothing);
+		transform.eulerAngles = smoothPos;
+	}
+
+
+}
